b42c6b0f859f0a788f4686d11a26014f3bb939c4
Peroli Sivaprakasam <peroli.sivaprakasam@csscorp.com>
2010-10-26 16:40:51 +0530

    Abstracted Address and Name fields as per Morty's suggestion to a new Editor Field, with all common behaviours.

---
+++ src/Controls/EditorField.js
@@ -0,0 +1,91 @@
+// todo: move to argos-saleslogix; this does not belong here.
+
+Ext.namespace('Sage.Platform.Mobile.Controls');
+
+(function() {
+    Sage.Platform.Mobile.Controls.EditorField = Ext.extend(Sage.Platform.Mobile.Controls.Field, {
+        template: new Simplate([
+            '<label for="{%= $.name %}">{%: $.label %}</label>',
+            '<a class="button whiteButton"><span>{%: $.lookupText %}</span></a>',
+            '<input type="text" />'
+        ]),
+        lookupText: '...',
+        emptyText: 'empty',
+        completeText: 'Ok',
+        formatter: function(val) {
+            return '';
+        },
+        init: function() {
+            Sage.Platform.Mobile.Controls.EditorField.superclass.init.apply(this, arguments);
+
+            this.containerEl.on('click', this.onClick, this, {stopEvent: true});
+        },        
+        createNavigationOptions: function() {
+            return {
+                tools: {
+                    tbar: [{
+                        name: 'complete',
+                        title: this.completeText,
+                        cls: 'button',
+                        fn: this.complete,
+                        scope: this
+                    }]
+                },
+                entry: this.originalValue,
+                changes: this.currentValue,
+                entityName: this.owner && this.owner.entityName
+            };
+        },
+        navigateToEditView: function() {
+            var view = App.getView(this.view),
+                options = this.createNavigationOptions();
+            if (view && options)
+                view.show(options);
+        },
+        onClick: function(evt, el, o) {
+            evt.stopEvent();
+
+            this.navigateToEditView();
+        },
+        complete: function() {
+            var view = App.getActiveView();
+            if (view)
+            {
+                this.currentValue = view.createEntry();
+
+                this.setText(this.formatter(view.getValues(true), true, true));
+
+                this.fireEvent('change', this.currentValue, this);
+            }
+
+            ReUI.back();
+        },
+        setText: function(text) {
+            this.el.dom.value = text;
+        },        
+        isDirty: function() {
+            return this.originalValue !== this.currentValue;
+        },
+        getValue: function() {
+            return this.currentValue;
+        },
+        setValue: function(val)
+        {            
+            if (val)
+            {
+                this.originalValue = this.currentValue = val;
+
+                this.setText(this.formatter(val, true, true));
+            }
+            else
+            {
+                this.originalValue = this.currentValue = null;
+
+                this.setText(this.emptyText);
+            }
+        },
+        clearValue: function() {
+            this.setValue(null);
+        }
+    });
+})();
--- src/Controls/NameField.js
+++ src/Controls/NameField.js
@@ -3,28 +3,19 @@
 Ext.namespace('Sage.Platform.Mobile.Controls');
 
 (function() {
-    Sage.Platform.Mobile.Controls.NameField = Ext.extend(Sage.Platform.Mobile.Controls.AddressField, {           
+    Sage.Platform.Mobile.Controls.NameField = Ext.extend(Sage.Platform.Mobile.Controls.EditorField, {
         template: new Simplate([
             '<label for="{%= $.name %}">{%: $.label %}</label>',
             '<a class="button whiteButton"><span>{%: $.lookupText %}</span></a>',
             '<input readonly="readonly" type="text" />'
         ]),
-        lookupText: '...',
         emptyText: 'no name',
         createNavigationOptions: function() {
-            return {
-                tools: {
-                    tbar: [{
-                        name: 'complete',
-                        title: this.completeText,
-                        cls: 'button',
-                        fn: this.complete,
-                        scope: this
-                    }]
-                },
-                entry: this.originalValue,
-                changes: this.currentValue
-            };
+            var options = Sage.Platform.Mobile.Controls.NameField.superclass.createNavigationOptions.apply(this, arguments);
+            //Name does not have an entity.
+            delete options.entityName;
+
+            return options;
         }
     });
 
