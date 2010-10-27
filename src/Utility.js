65e62c8cdaca3a6a56d3a2a251508af714deb366
mmorton <mmorton@icanhascode.com>
2010-10-27 15:11:37 -0700

    small compatability tweak.

--- src/Utility.js
+++ src/Utility.js
@@ -9,7 +9,7 @@ Ext.namespace('Sage.Platform.Mobile');
 Sage.Platform.Mobile.Utility = (function() {
     var nameToPathCache = {};
     var nameToPath = function(name) {
-        if (typeof name !== 'string' || name === '.') return [];        
+        if (typeof name !== 'string' || name === '.' || name === '') return []; // '', for compatibility  
         if (nameToPathCache[name]) return nameToPathCache[name];
         var parts = name.split('.');
         var path = [];
