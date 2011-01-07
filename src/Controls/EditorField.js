434f8ef67deada9fbb9db63c374d194bfd8dfe5f
Peroli Sivaprakasam <peroli.sivaprakasam@csscorp.com>
2011-01-07 13:57:43 +0530

    [#1-78800] Fixed a typo of property name in editorfield. Changed "name" property of toolbar to "id".

--- src/Controls/EditorField.js
+++ src/Controls/EditorField.js
@@ -39,7 +39,7 @@ Ext.namespace('Sage.Platform.Mobile.Controls');
             return {
                 tools: {
                     tbar: [{
-                        name: 'complete',
+                        id: 'complete',
                         fn: this.complete,
                         scope: this
                     },{
