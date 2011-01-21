1a1e2dd106325646d83e93a1adecf6428720fc0b
mmorton <mmorton@icanhascode.com>
2011-01-21 15:08:58 -0700

    updated build scripts.

--- build/release.cmd
+++ build/release.cmd
@@ -4,12 +4,12 @@ if exist deploy (
 	rmdir deploy /S /Q
 )
 
-mkdir deploy\app\content\ext
-mkdir deploy\app\content\datejs
-mkdir deploy\app\content\javascript
-mkdir deploy\app\content\images
-mkdir deploy\app\content\css
-mkdir deploy\app\content\reui
+mkdir deploy\content\ext
+mkdir deploy\content\datejs
+mkdir deploy\content\javascript
+mkdir deploy\content\images
+mkdir deploy\content\css
+mkdir deploy\content\reui
 
 tools\JsBit\jsbit.exe -p "build/release.jsb2" -d "."
 if %errorlevel% neq 0 exit /b %errorlevel%
