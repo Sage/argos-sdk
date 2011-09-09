6bdce274019c2beeed8623177bd7f2ab624015f5
mmorton <mmorton@icanhascode.com>
2011-09-09 11:13:47 -0700

    added java command line example to build script.

--- build/release.cmd
+++ build/release.cmd
@@ -11,5 +11,10 @@ mkdir deploy\content\images
 mkdir deploy\content\css
 mkdir deploy\content\reui
 
+REM .NET Build Tool
 tools\JsBit\jsbit.exe -p "build/release.jsb2" -d "."
+
+REM Java Build Tool
+REM %JAVA_HOME%\bin\java -Dfile.encoding=UTF-8 -jar "tools/JSBuilder/JSBuilder2.jar" -v -p "build/release.jsb2" -d "."
+
 if %errorlevel% neq 0 exit /b %errorlevel%
