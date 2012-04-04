9df19d8190b1564082ece2a5bbf24a1372480a4a
Tony Skidmore <tony.skidmore@sage.com>
2012-04-04 10:05:57 -0700

    added decode reference

--- src/Format.js
+++ src/Format.js
@@ -77,6 +77,7 @@ define('Sage/Platform/Mobile/Format', [
         minuteText: 'minute',
 
         encode: encode,
+        decode: decode,
         isEmpty: isEmpty,
         link: function(val) {
             if (typeof val !== 'string')
