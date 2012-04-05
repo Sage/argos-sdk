8ba0c4b28456c76c4f964f1eab659961899b8710
Michael Morton <mmorton@icanhascode.com>
2012-04-04 23:55:13 -0700

    working component mixin.

--- src/_Component.js
+++ src/_Component.js
@@ -1,59 +1,115 @@
-define('Sage/Platform/Mobile/_ComponentContainerMixin', [
+define('Sage/Platform/Mobile/_Component', [
     'dojo/_base/declare',
     'dojo/_base/array',
-    'dojo/_base/lang'
+    'dojo/_base/lang',
+    'dojo/_base/connect',
+    'dijit/_WidgetBase',
+    'dijit/_Container'
 ], function(
     declare,
     array,
-    lang
+    lang,
+    connect,
+    _WidgetBase,
+    _Container
 ) {
+    var parse = function(value) {
+        var segments = value.split(/\s*,\s*/),
+            map = {};
+
+        array.forEach(segments, function(segment) {
+            var pair = segment.split(':'),
+                event = lang.trim(pair[0]);
+
+            this[event] = pair[1] ? lang.trim(pair[1]) : event;
+        }, map);
+
+        return map;
+    };
+
     var _Component = declare('Sage.Platform.Mobile._Component', null, {
         owner: null,
-        create: function() {},
-        destroy: function() {}
-    });
+        _componentReady: false,
+        _componentSignals: null,
+        startup: function() {
+            this.initComponents();
+            this.inherited(arguments);
 
-    _Component.createComponent = function(component, options) {
-        var owner = options.owner,
-            type = component.type,
-            ctor = lang.getObject(type, false);
+            array.forEach(this.components, function(component) {
+                if (component.isInstanceOf(_WidgetBase) && component._started) return;
 
-        if (!ctor) throw new Error('Invalid component type.');
+                component.startup();
+            });
+        },
+        shutdown: function() {
+            array.forEach(this.components, function(component) {
+                if (component.isInstanceOf(_WidgetBase) && component._beingDestroyed) return;
 
-        var instance = new ctor(component.props);
-        if (instance.isInstanceOf(_Component))
-        {
-            instance.owner = owner;
-            instance.create();
-        }
+                component.destroy();
+            });
 
-    };
+            this.components = [];
+        },
+        destroy: function() {
+            array.forEach(this._componentSignals, function(signal) {
+                signal.remove();
+            });
+            this._componentSignals = null;
+            this.inherited(arguments);
+            this.shutdown();
+        },
+        initComponents: function() {
+            var created = [];
 
-    _Component.createComponents = function(components, options) {
-        options = options || {};
-
-        var creating = lang.isArray(components) ? components : [components],
-            created = [];
-
-        for (var i = 0; i < creating.length; i++)
-        {
-            var definition = creating[i],
-                owner = options.owner,
-                type = definition.type,
-                ctor = lang.getObject(type, false);
-
-            if (!ctor) throw new Error('Invalid component type.');
-
-            var instance = new ctor(definition.props);
-            if (instance.isInstanceOf(_Component))
+            for (var i = 0; i < this.components.length; i++)
             {
-                instance.owner = owner;
-                instance.create();
+                var component = this.components[i],
+                    type = component.type,
+                    ctor = lang.isFunction(type) ? type : lang.getObject(type, false);
+
+                if (!ctor) throw new Error('Invalid component type.');
+
+                var instance = new ctor(component.props);
+                if (instance.isInstanceOf(_Component))
+                    instance.owner = this;
+
+                created.push(instance);
+
+                var attach = component.attachPoint,
+                    events = component.attachEvent;
+
+                if (attach)
+                {
+                    if (lang.getObject(attach, false, this)) throw new Error('Attach point already occupied.');
+
+                    lang.setObject(attach, instance, this);
+                }
+
+                if (events && lang.isString(events))
+                    events = parse(events);
+
+                if (events)
+                {
+                    this._componentSignals = (this._componentSignals || []);
+
+                    for (var name in events)
+                    {
+                        this._componentSignals.push(connect.connect(instance, name, this, events[name]));
+                    }
+                }
+
+                if (instance.isInstanceOf(_WidgetBase))
+                {
+                    if (this.isInstanceOf(_Container))
+                        this.addChild(instance, component.position);
+                    else
+                        instance.placeAt(this.containerNode || this.domNode, component.position);
+                }
             }
 
-            created.push(instance);
+            this.components = created;
         }
+    });
 
-        return lang.isArray(components) ? created : created[0];
-    };
+    return _Component;
 });
