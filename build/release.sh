2480ac02fbbd01f0ba4c0db9eea7bc03b729c1af
Michael Morton <mmorton@icanhascode.com>
2012-07-23 16:41:57 -0700

    fixed build.

--- build/release.sh
+++ build/release.sh
@@ -4,15 +4,16 @@ if [ -d "deploy" ]; then
     rm -rf deploy
 fi
 
-mkdir -p deploy/content/ext
-mkdir -p deploy/content/datejs
 mkdir -p deploy/content/javascript
 mkdir -p deploy/content/images
 mkdir -p deploy/content/css
-mkdir -p deploy/content/reui
+mkdir -p deploy/content/dojo/dojo
+mkdir -p deploy/content/dojo/dojo/selector
+mkdir -p deploy/content/dojo/dijit
+mkdir -p deploy/content/dojo/dojox
 
 # .NET Build Tool
-# mono tools/JsBit/JsBit.exe -p "build/release.jsb2" -d "."
+mono tools/JsBit/JsBit.exe -p "build/release.jsb2" -d "."
 
 # Java Build Tool
-$JAVA_HOME/bin/java -Dfile.encoding=UTF-8 -jar "tools/JSBuilder/JSBuilder2.jar" -v -p "build/release.jsb2" -d "."
+# java -Dfile.encoding=UTF-8 -jar "tools/JSBuilder/JSBuilder2.jar" -v -p "build/release.jsb2" -d "."
